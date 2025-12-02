64f43d24489f0ed3fa5a6b73cd935d0549fb6143
Dmitry Rakhimov <dmitryrakhimov@Dmitrys-MacBook-Pro.local>
2025-12-02 23:12:03 +0200

    modified conftest file

--- conftest.py
+++ conftest.py
@@ -8,8 +8,7 @@ def driver():
     options = Options()
     options.add_argument('--headless')
     driver = webdriver.Firefox(options=options)
-    driver.implicitly_wait(5)
     driver.maximize_window()
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(5)
     yield driver
     driver.quit()
