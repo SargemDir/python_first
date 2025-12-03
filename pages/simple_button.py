bd98569adada9d340b4052ea3e8fd49af182c2f2
Dmitry Rakhimov <dmitryrakhimov@Dmitrys-MacBook-Pro.local>
2025-12-03 23:40:56 +0200

    modified page and test

--- pages/simple_button.py
+++ pages/simple_button.py
@@ -1,10 +1,15 @@
 from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
 
-button_selector = ()
+button_selector = (By.ID, 'submit-id-submit')
+result_selector = (By.ID, 'result-text')
 
 class SimpleButtonPage(BasePage):
     def __init__(self,driver):
         super().__init__(driver)
 
     def button(self):
-        return self.find()
+        return self.find(button_selector)
+
+    def result(self):
+        return self.find(result_selector)
--- test/test_button_page.py
+++ test/test_button_page.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.by import By
 
 def test_button1_exist(driver):
     driver.get('https://www.qa-practice.com/elements/button/simple')
-    assert driver.find_element(By.ID, 'sumbit-id-submit').is_displayed()
+    assert driver.find_element(By.ID, 'submit-id-submit').is_displayed()
 
 def test_button1_clicked(driver):
     driver.get('https://www.qa-practice.com/elements/button/simple')
-    driver.find_element(By.ID, 'sumbit-id-submit').click()
+    driver.find_element(By.ID, 'submit-id-submit').click()
     assert 'Submitted' == driver.find_element(By.ID, 'result-text').text
