dba978d7eb34097311c09f30cfcdd9098fc0f059
Dmitry Rakhimov <dmitryrakhimov@Dmitrys-MacBook-Pro.local>
2025-12-06 10:27:25 +0200

    modified files

--- pages/like_a_button.py
+++ pages/like_a_button.py
@@ -1,5 +1,31 @@
 from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
 
-class LideAButton(BasePage):
+
+button_selector = (By.LINK_TEXT, 'Click')
+result_selector = (By.ID, 'result-text')
+
+class LikeAButton(BasePage):
     def __init__(self,driver):
         super().__init__(driver)
+
+    def open(self):
+        self.driver.get('https://www.qa-practice.com/elements/button/like_a_button')
+
+    @property
+    def button(self):
+        return self.find(button_selector)
+
+    @property
+    def button_is_displayed(self):
+        return self.button.is_displayed()
+
+    def button_click(self):
+        self.button.click()
+    @property
+    def result(self):
+        return self.find(result_selector)
+
+    @property
+    def result_text(self):
+        return self.result.text
